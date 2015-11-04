53f3ed37da5747b6bfeebdbf99a48bc6f6e09ddf
rebservoir <reb_189@hotmail.com>
2015-11-04 14:22:29 -0700

    facade

--- config/app.php
+++ config/app.php
@@ -195,8 +195,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form'      => 'Collective\Html\FormFacade',
-        'Html'      => 'Collective\Html\HtmlFacade',
+        'Form'      => 'Illuminate\Html\FormFacade',
+        'Html'      => 'Illuminate\Html\HtmlFacade',
 
     ],
 
